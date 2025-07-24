a18e6919ce2869f3d549924586ab4a3a7df23adb
Divinecrusade <gorlovdima@inbox.ru>
2025-07-24 13:35:58 +0300

    feat: Finish chamber for test scene

--- Engine/TestScene.h
+++ Engine/TestScene.h
@@ -15,7 +15,9 @@ class TestScene : public IScene {
         CEILING_TEXTURE{Surface::FromFile(std::wstring{CEILING_TEXTURE_URL})},
         zbuffer{std::make_shared<std::unique_ptr<float[]>>(std::make_unique<float[]>(gfx.ScreenWidth * gfx.ScreenHeight))},
         pipe_static_planes{gfx, TextureLightEffect{}, zbuffer},
-        floor_object{Plain::GetSkinnedWithNormals<TextureLightEffect::TextureBindedVertex>(FLOOR_TESSALATION, FLOOR_TESSALATION, FLOOR_WIDTH, FLOOR_HEIGHT)} { 
+        floor_object{Plain::GetSkinnedWithNormals<TextureLightEffect::TextureBindedVertex>(FLOOR_TESSALATION, FLOOR_TESSALATION, FLOOR_WIDTH, FLOOR_HEIGHT)},
+        wall_object_long{Plain::GetSkinnedWithNormals<TextureLightEffect::TextureBindedVertex>(WALL_TESSALATION, WALL_TESSALATION, WALL_WIDTH_LONG, WALL_HEIGHT)},
+        wall_object_short{Plain::GetSkinnedWithNormals<TextureLightEffect::TextureBindedVertex>(WALL_TESSALATION, WALL_TESSALATION, WALL_WIDTH_SHORT, WALL_HEIGHT)}{ 
     pipe_static_planes.effect.vs.BindProjection(PROJECTION);
   }
 
@@ -72,6 +74,21 @@ class TestScene : public IScene {
     pipe_static_planes.effect.ps.BindTexture(FLOOR_TEXTURE);
     pipe_static_planes.effect.vs.BindWorldView(FLOOR_WORLD_POS * cur_view);
     pipe_static_planes.Draw(floor_object);
+    
+    pipe_static_planes.effect.ps.BindTexture(WALL_TEXTURE);
+    for (auto const& wall : WALLS_WORLD_POS) {
+      auto const& pos = wall.first;
+      auto const& type = wall.second;
+      pipe_static_planes.effect.vs.BindWorldView(pos * cur_view);
+      switch (type) {
+        case WALL_TYPE::LONG: pipe_static_planes.Draw(wall_object_long); break;
+        case WALL_TYPE::SHORT: pipe_static_planes.Draw(wall_object_short); break;
+      }
+    }
+
+    pipe_static_planes.effect.ps.BindTexture(CEILING_TEXTURE);
+    pipe_static_planes.effect.vs.BindWorldView(CEILING_WORLD_POS * cur_view);
+    pipe_static_planes.Draw(floor_object);
   }
 
  private:
@@ -83,23 +100,43 @@ class TestScene : public IScene {
   Surface const WALL_TEXTURE;
   Surface const CEILING_TEXTURE;
 
+  static constexpr std::size_t WALLS_COUNT{4ull};
+
   std::shared_ptr<std::unique_ptr<float[]>> zbuffer;
   Pipeline<TextureLightEffect> pipe_static_planes;
   IndexedTriangleList<TextureLightEffect::TextureBindedVertex> floor_object;
-  Vec3 light_pos{0.f, 0.f, 3.5f};
+  IndexedTriangleList<TextureLightEffect::TextureBindedVertex> wall_object_long;
+  IndexedTriangleList<TextureLightEffect::TextureBindedVertex> wall_object_short;
 
   static constexpr float dTheta = PI;
   static constexpr float wFOV = 90.f;
   static constexpr float screen_ratio = 4.f / 3.f;
   static constexpr float hFOV = wFOV / screen_ratio;
   static constexpr float _near = 0.2f;
-  static constexpr float _far = 8.f;
+  static constexpr float _far = 9.f;
   Mat4 const PROJECTION{Mat4::PerspectiveProjectionFromFOV(wFOV, screen_ratio, _near, _far)};
 
-  static constexpr float FLOOR_WIDTH = 8.f;
-  static constexpr float FLOOR_HEIGHT = 6.f;
+  static constexpr float FLOOR_WIDTH = 6.f;
+  static constexpr float FLOOR_HEIGHT = 8.f;
   static constexpr std::size_t FLOOR_TESSALATION = 10ull;
   Mat4 const FLOOR_WORLD_POS{Mat4::Identity() * Mat4::RotationX(PI / 2.f)};
+
+  static constexpr float WALL_WIDTH_LONG = FLOOR_HEIGHT;
+  static constexpr float WALL_WIDTH_SHORT = FLOOR_WIDTH;
+  static constexpr float WALL_HEIGHT = 5.f;
+  static constexpr std::size_t WALL_TESSALATION = 10ull;
+  enum WALL_TYPE {
+    LONG, SHORT
+  };
+  std::array<std::pair<Mat4, WALL_TYPE>, WALLS_COUNT> const WALLS_WORLD_POS{
+    std::pair<Mat4, WALL_TYPE>{Mat4::Identity() * Mat4::RotationY(-PI / 2.f) * Mat4::Translation({-FLOOR_WIDTH / 2.f, WALL_HEIGHT / 2.f, 0.f}), LONG},
+    std::pair<Mat4, WALL_TYPE>{Mat4::Identity() * Mat4::RotationY(PI / 2.f) * Mat4::Translation({FLOOR_WIDTH / 2.f, WALL_HEIGHT / 2.f, 0.f}), LONG},
+    std::pair<Mat4, WALL_TYPE>{Mat4::Identity() * Mat4::Translation({0.f, WALL_HEIGHT / 2.f, FLOOR_HEIGHT / 2.f}), SHORT},
+    std::pair<Mat4, WALL_TYPE>{Mat4::Identity() * Mat4::RotationY(-PI) * Mat4::Translation({0.f, WALL_HEIGHT / 2.f, -FLOOR_HEIGHT / 2.f}), SHORT}
+  };
+
+  Mat4 const CEILING_WORLD_POS{Mat4::Identity() * Mat4::RotationX(3.f * (PI / 2.f)) * Mat4::Translation({0.f, WALL_HEIGHT, 0.f})};
+
 
   Mat4 cur_view{Mat4::Identity()};
 
@@ -113,4 +150,6 @@ class TestScene : public IScene {
 
   static constexpr float camera_speed = 0.5f;
   Vec3 camera_pos{0.f, 1.6f, -FLOOR_HEIGHT / 2.f};
+
+  Vec3 light_pos{0.f, 0.f, 3.5f};
 };
